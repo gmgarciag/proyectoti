7ac4c92c35126ae31b3842ab778986cd769f77c2
sjvargas <sjvargas@uc.cl>
2016-05-02 15:02:10 -0300

    funcion comprar

--- proyecto/app/controllers/welcome_controller.rb
+++ proyecto/app/controllers/welcome_controller.rb
@@ -5,22 +5,55 @@ require 'base64'
 require 'cgi'
 require 'hmac-sha1'
   
- #metodo que envia la orden de compra para comprar la materia prima cuando no se tiene
-  def comprar(_sku,cantidad)
+def test
+  comprar(2,10)
+end
 
-if _sku != nil
-  
-  grupoProveedor=(Proveedor.find_by skuProducto: _sku).grupoProveedor
-  puts grupoProveedor
-  #asumo que ya tengo plata
-  #obtengo el ID del grupo
-  #IDgrupo=(IdGrupo.find_by numeroGrupo: grupoProveedor).idGrupo
+#metodo que envia la orden de compra para comprar la materia prima cuando no se tiene
+def comprar(_sku, cantidad)
 
-#llamada a crear la orden de compra, retorna la orden de compra o error
-  @ordenCompra=RestClient.put 'http://mare.ing.puc.cl/oc/crear/', {:cliente => '571262b8a980ba030058ab4f', :proveedor => '571262b8a980ba030058ab4f', :sku => 47, :fechaEntrega => 1463797342000, :cantidad => 4, :precioUnitario => 7244, :canal => 'b2b'}.to_json, :content_type => 'application/json'
+  if _sku != nil
+    
+    grupoProveedor=(Proveedor.find_by skuProducto: _sku).grupoProveedor # Si no existe el proveedor se cae
+    puts grupoProveedor
+    #asumo que ya tengo plata
+    #obtengo el ID del grupo
+    #IDgrupo=(IdGrupo.find_by numeroGrupo: grupoProveedor).idGrupo
 
+    #llamada a crear la orden de compra, retorna la orden de compra o error
+    #@ordenCompra=RestClient.put 'http://mare.ing.puc.cl/oc/crear/', {:cliente => '571262b8a980ba030058ab4f', :proveedor => '571262b8a980ba030058ab4f', :sku => 47, :fechaEntrega => 1463797342000, :cantidad => 4, :precioUnitario => 7244, :canal => 'b2b'}.to_json, :content_type => 'application/json'
+    
+    # Added stuff...
+
+    # Get the group id from the number
+    idgrupo=(IdGrupo.find_by numeroGrupo: grupoProveedor).idGrupo # Si no existe el idGrupo se cae, para prbar debes poblar la databse
+    puts "idgrupo: " + idgrupo
+    # Generate the OC
+    @ordenCompra=RestClient.put 'http://mare.ing.puc.cl/oc/crear/', {:cliente => '571262b8a980ba030058ab4f', :proveedor => idgrupo, :sku => _sku, :fechaEntrega => 1463797342000, :cantidad => cantidad, :precioUnitario => 7244, :canal => 'b2b'}.to_json, :content_type => 'application/json'
+    # Get the other group link
+    oc = ActiveSupport::JSON.decode(@ordenCompra) # http://stackoverflow.com/questions/5348449/get-a-particular-key-value-from-json-in-ruby
+    url = 'http://integra'+grupoProveedor.to_s+'.ing.puc.cl/api/oc/recibir/' + oc['_id']
+    puts "URL GRUPO: " + url
+
+    # Send the OC to he other group
+    #@anwer=RestClient.post url, @ordenCompra, :content_type => 'application/json'
+    @answer = {aceptado: false, idoc: oc['_id']}.to_json # As long as othe groups don't implemen it, just to test. When implemented, use the code above this line.
+    # answer to json
+    ans = ActiveSupport::JSON.decode(@answer)
+    # Check if positive answer
+    if ans['aceptado']
+      # They accepted it, YEI!
+      # We are done here, they need to create the factura and send it back. We continue there.
+      render :json => @answer
+    else
+      # They refused it :( Let's cancel the OC
+      url = 'http://mare.ing.puc.cl/oc/anular/'+oc['_id']
+      body = {id: oc['_id'], anulacion: 'OC rejected'}.to_json
+      #Delete method doesn't accept body, so we need an advanced way of executing the code:
+      @result = RestClient::Request.execute(method: :delete, url: url, payload: body, headers: {:content_type => 'application/json'})
+      render :json => @result
+    end
   end
-
 end
 
   def index
